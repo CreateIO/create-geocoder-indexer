4c3cc5b890f89102912fc3ba7913a3449a427d09
Carl Anderson <carl.anderson@vadose.org>
2015-12-31 10:36:49 -0500

    protect against trouble

--- bin/builder_finalize.sh
+++ bin/builder_finalize.sh
@@ -11,12 +11,25 @@ if [ ! -r bin/batch_pre.sh ]; then
     exit
 fi
 
+if [ -r data/tmp ]; then
+    echo "removing working data before packaging the build"
+    rm -rf data/tmp
+fi
+if [ -r data/batch_apply.sh -o -r data/batch_pre.sh ]; then
+    echo "removing scripts from the data dir before packaging the build"
+    rm -f data/batch_apply.sh data/batch_pre.sh
+fi
+
+
 # do not save directory paths and compress the file as much as possible
-zip -j -9 dumps/${file}  bin/batch_apply.sh bin/batch_pre.sh data/*
+zip -j -9 dumps/${file}  data/* bin/batch_apply.sh bin/batch_pre.sh 
 
 # save the build into AWS S3
 # you may need to set the AWS_KEY and AWS_SECRET first
+echo "uploading build to AWS S3"
 aws s3 cp dumps/${file} s3://t-rex-dumps/geocoder/${file}
 
-echo "now run batch_apply.sh to apply the data"
+echo "now unpackage the dump into the data dir "
+echo "  unzip -d data dumps/$file"
+echo "then run data/batch_apply.sh to apply the data"
 
