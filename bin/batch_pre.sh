b59ab52ecd88b72bcf8c8d15737f1db392782b89
Carl Anderson <carl.anderson@vadose.org>
2016-06-27 08:49:09 -0400

    port and market abstractions for batch_pre loader

--- bin/batch_pre.sh
+++ bin/batch_pre.sh
@@ -1,49 +1,51 @@
-#ES_Index=geodc_c
+#ES_Index=${idxName}
 
-curl -XDELETE 'http://localhost:9200/geodc_c/address'
+idxName="geodc_c"
 
-
-curl -XPOST 'http://localhost:9200/geodc_c/address' -d '{"mappings": {"address": {"properties": {"city": {"type": "string"}, "neighborhood": {"type": "string"}, "address_number": {"type": "integer"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "zipcode": {"type": "string"}, "name_type": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}, "addr_use": {"type": "string"}}}}}'
-
-
-curl -XDELETE 'http://localhost:9200/geodc_c/nbhd'
+curl -XDELETE "http://localhost:${ES_LOAD_PORT}/${idxName}/address"
 
 
-curl -XPOST 'http://localhost:9200/geodc_c/nbhd' -d '{"mappings": {"nbhd": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}}}}}'
+curl -XPOST "http://localhost:${ES_LOAD_PORT}/${idxName}/address" -d '{"mappings": {"address": {"properties": {"city": {"type": "string"}, "neighborhood": {"type": "string"}, "address_number": {"type": "integer"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "zipcode": {"type": "string"}, "name_type": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}, "addr_use": {"type": "string"}}}}}'
 
 
-curl -XDELETE 'http://localhost:9200/geodc_c/landmark'
+curl -XDELETE "http://localhost:${ES_LOAD_PORT}/${idxName}/nbhd"
 
 
-curl -XPOST 'http://localhost:9200/geodc_c/landmark' -d '{"mappings": {"landmark": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "zipcode": {"type": "string"}, "name_type": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}, "addr_use": {"type": "string"}}}}}'
+curl -XPOST "http://localhost:${ES_LOAD_PORT}/${idxName}/nbhd" -d '{"mappings": {"nbhd": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}}}}}'
 
 
-curl -XDELETE 'http://localhost:9200/geodc_c/SMC'
+curl -XDELETE "http://localhost:${ES_LOAD_PORT}/${idxName}/landmark"
 
 
-curl -XPOST 'http://localhost:9200/geodc_c/SMC' -d '{"mappings": {"SMC": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}}}}}'
+curl -XPOST "http://localhost:${ES_LOAD_PORT}/${idxName}/landmark" -d '{"mappings": {"landmark": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "zipcode": {"type": "string"}, "name_type": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}, "addr_use": {"type": "string"}}}}}'
 
 
-curl -XDELETE 'http://localhost:9200/geodc_c/SMR'
+curl -XDELETE "http://localhost:${ES_LOAD_PORT}/${idxName}/SMC"
 
 
-curl -XPOST 'http://localhost:9200/geodc_c/SMR' -d '{"mappings": {"SMR": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}}}}}'
+curl -XPOST "http://localhost:${ES_LOAD_PORT}/${idxName}/SMC" -d '{"mappings": {"SMC": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}}}}}'
 
 
-curl -XDELETE 'http://localhost:9200/geodc_c/market'
+curl -XDELETE "http://localhost:${ES_LOAD_PORT}/${idxName}/SMR"
 
 
-curl -XPOST 'http://localhost:9200/geodc_c/market' -d '{"mappings": {"market": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}}}}}'
+curl -XPOST "http://localhost:${ES_LOAD_PORT}/${idxName}/SMR" -d '{"mappings": {"SMR": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}}}}}'
 
 
-curl -XDELETE 'http://localhost:9200/geodc_c/postalcode'
+curl -XDELETE "http://localhost:${ES_LOAD_PORT}/${idxName}/market"
 
 
-curl -XPOST 'http://localhost:9200/geodc_c/postalcode' -d '{"mappings": {"postalcode": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}}}}}'
+curl -XPOST "http://localhost:${ES_LOAD_PORT}/${idxName}/market" -d '{"mappings": {"market": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}}}}}'
 
 
-curl -XDELETE 'http://localhost:9200/geodc_c/quadrant'
+curl -XDELETE "http://localhost:${ES_LOAD_PORT}/${idxName}/postalcode"
 
 
-curl -XPOST 'http://localhost:9200/geodc_c/quadrant' -d '{"mappings": {"quadrant": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}}}}}'
+curl -XPOST "http://localhost:${ES_LOAD_PORT}/${idxName}/postalcode" -d '{"mappings": {"postalcode": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}}}}}'
+
+
+curl -XDELETE "http://localhost:${ES_LOAD_PORT}/${idxName}/quadrant"
+
+
+curl -XPOST "http://localhost:${ES_LOAD_PORT}/${idxName}/quadrant" -d '{"mappings": {"quadrant": {"properties": {"city": {"type": "string"}, "complete_address": {"type": "string"}, "core_address": {"type": "string"}, "state": {"type": "string"}, "alt_core_address": {"type": "string"}, "super_core_address": {"type": "string"}}}}}'
 
